665caa093777b39e66540492b70f902419893b59
Raissa Scheeren <raissa.fofs@gmail.com>
2020-04-29 18:05:31 -0300

    vuforia working + basic assets & logic

---
+++ Assets/Script/BeatScroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour {
+    public float beatTempo; //actual music tempo
+
+    void Start () {
+        //conversion for Unity frames, adjusting the speed goes per second along
+        //example: 120 tempo - 2 units per second
+        beatTempo = beatTempo / 60f;
+    }
+
+    void Update () {
+        transform.position -= new Vector3 (0f, beatTempo * 10 * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/Script/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+    public Text debugText;
+    private GameObject onButtonArea;
+
+    void Start(){
+        onButtonArea = null;
+    }
+
+    void Update () {
+        debugText.text = "Position: " + transform.position;
+    }
+
+    public void OnTriggerEnter2D (Collider2D collider2D) {
+        Debug.Log("Pegou nota");
+        //storing object in the button area
+        onButtonArea = collider2D.gameObject;
+    }
+
+    public void OnTriggerExit2D (Collider2D collider2D) {
+        Debug.Log("Saiu da área");
+        //taking object out of the button area
+        onButtonArea = null;
+    }
+
+    public void IsPressed () {
+        Debug.Log("Pressionou! onButtonArea: " + onButtonArea);
+        //checks if object is on button area
+        if (onButtonArea) {
+            float noteDistance = Vector3.Distance(transform.position, onButtonArea.transform.position);
+            debugText.text = "Pegou nota, dist: " + noteDistance;
+        }
+        else{
+            debugText.text = "deu ruim, sem nota";
+        }
+    }
+}
